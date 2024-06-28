f7ae5640cae2ea3225af223bc07f35370c78994e
chaonanwang-zeal <109785407+chaonanwang-zeal@users.noreply.github.com>
2024-06-28 21:19:42 +0900

    Update bigquery.tf

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,7 +1,7 @@
 resource "google_bigquery_table" "terra_table" {
   dataset_id = "oh_practice"
   table_id   = "terra_table"
-
+  deletion_protection = false  // 添加这一行
   schema = jsonencode([
     {
       name = "column1",
@@ -16,18 +16,15 @@ resource "google_bigquery_table" "terra_table" {
       type = "INTEGER"
     }
   ])
-
-  deletion_protection = false
 }
 
 resource "google_bigquery_table" "terra_view" {
   dataset_id = "oh_practice"
   table_id   = "terra_table_view"
-
+  deletion_protection = false  // 添加这一行
   view {
     query          = "SELECT column1, column2 FROM `${google_bigquery_table.terra_table.project}.${google_bigquery_table.terra_table.dataset_id}.${google_bigquery_table.terra_table.table_id}`"
     use_legacy_sql = false
   }
-
   depends_on = [google_bigquery_table.terra_table]
 }
