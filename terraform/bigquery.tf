5498b02bd3767ec93024374aaa54b687754c5e2c
chaonanwang-zeal <109785407+chaonanwang-zeal@users.noreply.github.com>
2024-06-28 21:29:41 +0900

    drop column

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,14 +1,10 @@
 resource "google_bigquery_table" "terra_table" {
   dataset_id = "oh_practice"
   table_id   = "terra_table"
-  deletion_protection = false  // 添加这一行
+
   schema = jsonencode([
     {
       name = "column1",
-      type = "STRING"
-    },
-    {
-      name = "column2",
       type = "STRING"
     },
     {
@@ -21,7 +17,7 @@ resource "google_bigquery_table" "terra_table" {
 resource "google_bigquery_table" "terra_view" {
   dataset_id = "oh_practice"
   table_id   = "terra_table_view"
-  deletion_protection = false  // 添加这一行
+
   view {
     query          = "SELECT column1, column2 FROM `${google_bigquery_table.terra_table.project}.${google_bigquery_table.terra_table.dataset_id}.${google_bigquery_table.terra_table.table_id}`"
     use_legacy_sql = false
