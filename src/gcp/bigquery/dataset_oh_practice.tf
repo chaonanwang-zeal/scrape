8d3135de29cc0fe442cdbf1551e7db5a3d3e4714
chaonanwang-zeal <109785407+chaonanwang-zeal@users.noreply.github.com>
2024-06-29 19:38:21 +0900

    Update dataset_oh_practice.tf

--- src/gcp/bigquery/dataset_oh_practice.tf
+++ src/gcp/bigquery/dataset_oh_practice.tf
@@ -11,6 +11,10 @@ resource "google_bigquery_table" "terra_table" {
     {
       name = "column2",
       type = "INTEGER"
+    },
+    {
+      name = "column3",
+      type = "INTEGER"
     }
   ])
 }
