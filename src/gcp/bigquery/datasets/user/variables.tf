35015f11194122d0d300bb7c460ee674fba169f0
chaonanwang-zeal <109785407+chaonanwang-zeal@users.noreply.github.com>
2024-06-29 18:45:38 +0900

    Delete src/gcp/bigquery/datasets directory

--- src/gcp/bigquery/datasets/user/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "Region for the GCP resources"
-  type        = string
-}
