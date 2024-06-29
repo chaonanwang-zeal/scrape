ac05c898d84202e41c429db6fbb9ad299c195680
chaonanwang-zeal <109785407+chaonanwang-zeal@users.noreply.github.com>
2024-06-29 20:31:50 +0900

    Update dataset_oh_practice.tf

--- src/gcp/bigquery/dataset_oh_practice.tf
+++ src/gcp/bigquery/dataset_oh_practice.tf
@@ -22,12 +22,4 @@ resource "google_bigquery_table" "terra_table" {
   ])
 }
 
-resource "google_bigquery_table" "terra_view" {
-  dataset_id = "oh_practice"
-  table_id   = "terra_table_view"
-  view {
-    query          = "SELECT column1, column2 FROM `${google_bigquery_table.terra_table.project}.${google_bigquery_table.terra_table.dataset_id}.${google_bigquery_table.terra_table.table_id}`"
-    use_legacy_sql = false
-  }
-  depends_on = [google_bigquery_table.terra_table]
-}
+
