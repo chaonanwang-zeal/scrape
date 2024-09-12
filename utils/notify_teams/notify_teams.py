0c8b99631d1b8a09cc8d1904f2b1222fe8f78ff6
chaonanwang <chaonan.wang@zdh.co.jp>
2024-09-12 15:36:32 +0900

    added

--- utils/notify_teams/notify_teams.py
+++ utils/notify_teams/notify_teams.py
@@ -2,7 +2,7 @@ import os
 import requests
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.gcp.composer.mention_members import MENTION_MEMBERS
 # from mention_members import MENTION_MEMBERS
 
